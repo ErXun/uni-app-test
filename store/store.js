ee596c0c4ee4f40c2382a814d1de567d1e1fd7a9
erxun <erxun.fighting@gmail.com>
2025-08-14 16:13:34 +0800

    add cart

--- store/store.js
+++ store/store.js
@@ -18,6 +18,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import moduleCart from './cart.js'
+import moduleUser from './user.js'
 
 // 2. 将 Vuex 安装为 Vue 的插件
 Vue.use(Vuex)
@@ -27,6 +28,7 @@ const store = new Vuex.Store({
   // TODO：挂载 store 模块
   modules: {
      m_cart: moduleCart,
+     m_user: moduleUser,
   },
 })
 
